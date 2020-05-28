8c832bc95d36d2f4e2caed84450d631f5a368dac
JeremyMIQUEL <jeremy.miquel@yahoo.fr>
2020-05-28 19:38:09 +0200

    nav-bar done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_192233) do
+ActiveRecord::Schema.define(version: 2020_05_26_185725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 2020_05_26_192233) do
     t.text "description"
     t.decimal "price"
     t.string "img_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "transactions", force: :cascade do |t|
-    t.integer "duration"
-    t.boolean "validation"
-    t.string "review"
-    t.string "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
