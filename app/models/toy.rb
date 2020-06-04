82fffdedbd0437a6100567bafdfb44dce2b1d382
Julie Bouvier <julie.bouvier07@hotmail.fr>
2020-06-04 19:33:16 +0200

    changer en commentaire

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,13 @@
 class Toy < ApplicationRecord
-end
+#   validates :name, :photos, presence: true
+#   has_many :transactions #, dependent: :destroy
+#   belongs_to :owner , class_name: "User", foreign_key: "user_id"
+#   has_many_attached :photos
+
+
+#   def unavailable_dates
+#     transactions.pluck(:date_start, :date_end).map do |range|
+#       { from: range[0], to: range[1] }
+#     end
+#   end
+# end
