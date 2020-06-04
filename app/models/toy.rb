d2ffddd3dd7af11350225062e86f758fa11d845e
Julie Bouvier <julie.bouvier07@hotmail.fr>
2020-06-04 20:51:35 +0200

    done

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,4 +1,5 @@
 class Toy < ApplicationRecord
+end
 #   validates :name, :photos, presence: true
 #   has_many :transactions #, dependent: :destroy
 #   belongs_to :owner , class_name: "User", foreign_key: "user_id"
