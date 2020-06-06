4844f6c6e481c8fd1ad21059f62b2d1ab95eef2b
Paul M2B <polo2bodi@gmail.com>
2020-06-06 14:33:11 +0200

    Add relations between models

---
+++ db/migrate/20200606121907_add_references_to_transaction_and_toys.rb
@@ -0,0 +1,7 @@
+class AddReferencesToTransactionAndToys < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :transactions, :toy, foreign_key: true
+    add_reference :transactions, :customer, index: { unique: true }
+    add_reference :toys, :user, foreign_key: true
+  end
+end
